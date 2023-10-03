de7db146ed4a2a0aa3722dc9abe7ee9536d0e31d
pctimhk <pctimhk@gmail.com>
2023-10-03 15:20:04 +0800

    Create auditable model interface

---
+++ EFCAuditableModel/IAuditableModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuditableEFModel
+{
+    /// <summary>
+    /// Reference: https://nwb.one/blog/auditing-dotnet-entity-framework-core
+    /// </summary>
+    public interface IAuditableModel
+    {
+
+        DateTime CreatedDateTime { get; set; }
+        String CreatedBy { get; set; }
+
+        DateTime ModifiedDateTime { get; set; }
+        String ModifiedBy { get; set; }
+
+        String ModifiedChannel { get; set; }
+    }
+}
